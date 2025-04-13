b37af6ca2dba947d3bbdefad638430fe800037e2
Racha <rachaghariani506@gmail.com>
2025-04-13 17:21:01 +0100

    update configuration Security first commit

--- projet_ams_backend_2025/src/main/java/com/sip/ams/configuration/SecurityConfiguration.java
+++ projet_ams_backend_2025/src/main/java/com/sip/ams/configuration/SecurityConfiguration.java
@@ -49,8 +49,8 @@ public class SecurityConfiguration {
 	    public CorsConfigurationSource corsConfigurationSource() {
 	        CorsConfiguration configuration = new CorsConfiguration();
 	        
-	      //  configuration.setAllowedOrigins(Arrays.asList("http://jenkins.sip-academy.com:8070")); // URL Angular exacte
-	        	        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8070")); // URL Angular exacte
+	      configuration.setAllowedOrigins(Arrays.asList("http://jenkins.sip-academy.com:8070")); // URL Angular exacte
+	        	     //     configuration.setAllowedOrigins(Arrays.asList("http://localhost:8070")); // URL Angular exacte
 			configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	        configuration.setAllowedHeaders(Arrays.asList("*"));
 	        configuration.setAllowCredentials(true);
